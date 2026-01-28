baf96dd7236990adc7e48d627bd13966430b9522
Yasinwahab127 <yasinwahab133@gmial.com>
2026-01-28 21:39:07 +0500

    Added

---
+++ script.js
@@ -0,0 +1,61 @@
+        // hello word Meassage
+
+// 1) usign the alert method
+// alert("Hello World");
+
+//2) another method of .log
+console.log("Hello World");
+
+
+
+        // Use Variables (var, let, const)
+
+var yourname = "Mohsen Rehman";
+let yourcountry = "Pakistan";
+const fvrgame = "Cricket";
+let age = 20;
+console.log(yourname);
+console.log(yourcountry);
+console.log(fvrgame);
+console.log(age);
+
+
+        // print the data on webpage
+
+// let yourname = "Mohsen Rehman";
+// document.writeln("Your name: " + yourname);
+
+
+        // Change the value of a let variable 
+
+// let Firstname = "khan"
+// let agee = 20;
+
+
+// Firstname = "Change from khan to mohsen"
+// agee = 18;
+// console.log(Firstname); //old value
+// console.log(agee);   //old  value
+
+// console.log(Firstname); //updateed valuee
+// console.log("updated age:" ,agee); //updated value
+
+        // now lets try to change the const variable value
+
+// const country = "Pakistan";
+// const city = "Peshawat";
+// const number = 1234;
+
+// country = "Afghanistan";
+// city = "kabal";
+// number=321;
+
+// console.log(country);   //old value 
+// console.log(city);     // old value
+// console.log(number);    //old value
+
+// console.log(country);   //updated value
+// console.log(city);      
+// console.log(number);    
+// it show error beacuse when we assign some value to the const variable so it cannot be changed
+
